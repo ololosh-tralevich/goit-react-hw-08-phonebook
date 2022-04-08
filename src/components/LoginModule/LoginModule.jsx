564f75b1e4937a86e27ca6e02702f24febcffb19
ololosh-tralevich <karpeko76@gmail.com>
2022-04-08 15:00:09 +0100

    upd

--- src/components/LoginModule/LoginModule.jsx
+++ src/components/LoginModule/LoginModule.jsx
@@ -2,13 +2,17 @@ import style from './loginModule.module.css';
 
 const LoginModule = () => {
   return (
-    <form>
-      <label htmlFor="email">Your Email:</label>
-      <input name="email" type="email" id='email' />
+    <div className={style.loginCont}>
+      <h3 className={style.loginContTilte}>Log In</h3>
+      <form className={style.loginForm}>
+        <label htmlFor="email">Your Email:</label>
+        <input name="email" type="email" id="email" required/>
 
-      <label htmlFor="password">Your Password:</label>
-      <input name="password" type="password" id='password' />
-    </form>
+        <label htmlFor="password">Your Password:</label>
+        <input name="password" type="password" id="password" required/>
+        <button className={style.formSubmitBtn} type='submit'>Log In</button>
+      </form>
+    </div>
   );
 };
 
--- src/components/RegisterModule/RegisterModule.jsx
+++ src/components/RegisterModule/RegisterModule.jsx
@@ -2,16 +2,22 @@ import style from './registerModule.module.css';
 
 const RegisterModule = () => {
   return (
-    <form>
-        <label htmlFor='name'>Your Name:</label>
-      <input name='name' type="text" id='name'/>
+    <div className={style.registerCont}>
+      <h3 className={style.regContTitle}>Registration</h3>
+      <form className={style.registerForm}>
+        <label htmlFor="name">Your Name:</label>
+        <input name="name" type="text" id="name" required />
 
-      <label htmlFor='email'>Your Email:</label>
-      <input name='email' type="email" id='email'/>
+        <label htmlFor="email">Your Email:</label>
+        <input name="email" type="email" id="email" required />
 
-      <label htmlFor='password'>Your Password:</label>
-      <input name='password' type="password" id='password'/>
-    </form>
+        <label htmlFor="password">Your Password:</label>
+        <input name="password" type="password" id="password" required />
+        <button type="submit" className={style.sendFormBtn}>
+          Sign In
+        </button>
+      </form>
+    </div>
   );
 };
 
